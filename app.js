a577c03200ece0cd79ee6e7184bec9325b04164a
Osmin <osminv91@gmail.com>
2023-09-28 18:32:23 -0500

    Getting... routers enpoit

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require('express')
 const cookieparse = require('cookie-parse') 
 const app = express()
+
+
+const director = require('./routers/director')
 const genero = require('./routers/genero')
+const productora = require('./routers/')
+const tipo = require('./routers/tipo')
 const testing = require('./routers/test')
 
+app.use('/api/v1/directors',director)
 app.use('/api/v1/generos',genero)
+app.use('/api/v1/productors',productora)
+app.use('/api/v1/tipys',tipo)
 app.use('/api/v1/testing',testing)
+
 app.use(express.json())
 app.use(express.urlencoded({extends:true}))
 
